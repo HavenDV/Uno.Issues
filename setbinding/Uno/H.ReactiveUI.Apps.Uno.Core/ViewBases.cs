f606a1c92e1b7fc73173f056c2c956cfd7dfe498
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-07-13 21:21:26 +1000

    fix: Removed Source generator from setbinding issue.

---
+++ setbinding/Uno/H.ReactiveUI.Apps.Uno.Core/ViewBases.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+namespace HReactiveUI.Apps.Views
+{
+    public abstract partial class FileInteractionsViewBase
+    : global::ReactiveUI.Uno.ReactiveUserControl<global::H.ReactiveUI.Apps.ViewModels.FileInteractionsViewModel>
+    {
+    }
+
+    public abstract partial class MainViewBase
+    : global::ReactiveUI.Uno.ReactivePage<global::H.ReactiveUI.Apps.ViewModels.MainViewModel>
+    {
+    }
+
+    public abstract partial class MessageInteractionsViewBase
+    : global::ReactiveUI.Uno.ReactiveUserControl<global::H.ReactiveUI.Apps.ViewModels.MessageInteractionsViewModel>
+    {
+    }
+
+    public abstract partial class PreviewDropViewBase
+    : global::ReactiveUI.Uno.ReactiveUserControl<global::H.ReactiveUI.Apps.ViewModels.PreviewDropViewModel>
+    {
+    }
+
+    public abstract partial class WebInteractionsViewBase
+    : global::ReactiveUI.Uno.ReactiveUserControl<global::H.ReactiveUI.Apps.ViewModels.WebInteractionsViewModel>
+    {
+    }
+}
