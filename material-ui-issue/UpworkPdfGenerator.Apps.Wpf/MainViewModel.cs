916687ac0ddade6692ef8c5703f0c4232dfe387c
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-08-26 18:57:47 +1000

    feat: Added material-ui.issue.

---
+++ material-ui-issue/UpworkPdfGenerator.Apps.Wpf/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Drawing;
+
+namespace UpworkPdfGenerator.Apps.Wpf;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private System.Drawing.Color color = System.Drawing.Color.Red;
+
+    [ObservableProperty]
+    private System.Drawing.Color color2 = System.Drawing.Color.Blue;
+
+    partial void OnColorChanged(Color value)
+    {
+        Color2 = value;
+    }
+}
---
+++ material-ui-issue/UpworkPdfGenerator.Apps.Wpf/SystemDrawingColorToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using System.Globalization;
+using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace UpworkPdfGenerator.Apps.Wpf;
+
+[ValueConversion(typeof(System.Drawing.Color), typeof(Color))]
+public class SystemDrawingColorToColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not System.Drawing.Color expected)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return Color.FromArgb(expected.A, expected.R, expected.G, expected.B);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Color expected)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return System.Drawing.Color.FromArgb(expected.A, expected.R, expected.G, expected.B);
+    }
+}
