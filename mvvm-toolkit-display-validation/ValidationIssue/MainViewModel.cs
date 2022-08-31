cbd1605c8928a46f51e181717f01430bfe2bf61c
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-08-31 11:06:58 +1000

    feat: Added ValidationIssue.

---
+++ mvvm-toolkit-display-validation/ValidationIssue/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using ValidationIssue;
+
+namespace UpworkPdfGenerator.Apps.Wpf;
+
+public partial class MainViewModel : ObservableValidator
+{
+    [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [Required(
+        ErrorMessageResourceType = typeof(ValidationResources),
+        ErrorMessageResourceName = nameof(ValidationResources.Required))]
+    [Display(
+        ResourceType = typeof(ValidationResources),
+        Name = nameof(ValidationResources.Username))]
+    private string username = string.Empty;
+
+    public string[] Languages { get; } = new[]
+    {
+        "en-US",
+        "de-DE",
+        "es-ES",
+        "fr-FR",
+        "zh-CN",
+    };
+
+    [ObservableProperty]
+    private string selectedLanguage = string.Empty;
+
+    public MainViewModel()
+    {
+        SelectedLanguage = Languages.First();
+    }
+
+    partial void OnSelectedLanguageChanged(string value)
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(value);
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(value);
+
+        ValidateAllProperties();
+    }
+}
